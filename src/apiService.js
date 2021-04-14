aa59681aef33bb00d961e8911dbb964dbc2a4953
MrPaul007 <75275133+MrPaul007@users.noreply.github.com>
2021-04-14 15:59:33 +0300

    ready

--- src/apiService.js
+++ src/apiService.js
@@ -1,1 +1,15 @@
-https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=20735432-c0677fc566250abe2f2f1af55
+const ref = {
+    API_KEY: '20735432-c0677fc566250abe2f2f1af55',
+    URL: `https://pixabay.com/api/`,
+  
+    page: 0,
+    search: '',
+  
+    fetchApi(name) {
+      return fetch(
+        `${this.URL}/?image_type=photo&orientation=horizontal&q=${name}&page=${this.page}&per_page=12&key=${this.API_KEY}`,
+      ).then(res => res.json());
+    },
+  };
+  
+  export default ref;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './styles.css';
+
+import { debounce } from 'debounce';
+
+import ref from './apiService.js';
+import photoCardTpl from './photoCard.hbs';
+
+const searchFormEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('[data-action="load-more"]');
+
+function renderImage() {
+  ref.fetchApi(ref.search).then(data => {
+    const dataEl = data.hits.reduce((acc, item) => {
+      return acc + photoCardTpl(item);
+    }, '');
+    galleryEl.insertAdjacentHTML('beforeend', dataEl);
+    if (data.hits.length < 12) {
+      btnLoadMore.classList.add('is-hidden');
+    }
+console.log(document.documentElement.scrollHeight/2);
+    window.scrollTo({
+      top:document.documentElement.scrollHeight - 1390,
+      behavior: 'smooth',
+    });
+  });
+}
+
+function searchImg(e) {
+  ref.page = 1;
+  ref.search = '';
+  galleryEl.innerHTML = '';
+
+  btnLoadMore.classList.remove('is-hidden');
+
+  ref.search = e.target.value;
+  renderImage();
+}
+
+function onLoadMore() {
+  ref.page += 1;
+
+  renderImage();
+}
+
+searchFormEl.addEventListener('input', debounce(searchImg, 500));
+
+btnLoadMore.addEventListener('click', onLoadMore);
